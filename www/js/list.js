686bd721d29fa8d55c4684faa2a68c2f4a91700a
edilsonJunior <ed.junior.tecnologia@gmail.com>
2015-01-30 16:37:15 -0200

    Primeiro commit

---
+++ www/js/list.js
@@ -0,0 +1,57 @@
+
+
+angular.module('crud', [])
+
+.controller('create', function($scope, $cordovaSQLite) {
+  
+ 
+    $scope.add = function(){
+           
+        var query = "INSERT INTO tabela(nome)VALUES ('"+$scope.nome+"');";
+        $cordovaSQLite.execute(db, query).then(function(res) {
+            console.log("insertId: " + res.insertId);
+
+              window.alert("Usuario cadastrado");
+             
+              }, function (err) {
+                  console.error(err);
+              });
+
+ window.alert("Usuario cadastrado");
+
+
+ }
+
+});
+
+
+/*
+angular.module('crud', [''])
+
+.controller('create', function($scope, $cordovaSQLite) {
+  
+    $scope.add = function(){
+     window.alert("Usuario cadastrado");
+        
+        /*var query = "INSERT INTO teste (nome, email) VALUES ('a','b')";
+        $cordovaSQLite.execute(db, query, ["test", 100]).then(function(res) {
+        console.log("insertId: " + res.insertId);
+
+              window.alert("Usuario cadastrado");
+             
+              }, function (err) {
+                  console.error(err);
+              });
+
+
+
+   itens.push( {nome: $scope.nome});
+    
+    //window.alert(itens[scope.nome]);
+    contador = contador+ 1;   
+    }
+});
+
+
+
+*/
